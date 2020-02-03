c8a5dcc03e971c83a9f7c2f5bc95d9a9f8b2c2a6
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-02-03 16:53:16 +0000

    ianhinderslaptop/setup.sh: Add some flags - not working yet

--- codes/LUMA/configs/ianhinderslaptop/setup.sh
+++ codes/LUMA/configs/ianhinderslaptop/setup.sh
@@ -5,3 +5,12 @@ echo "Loading Ian-Hinders-MBP.sh"
 if [ -z "${OMP_NUM_THREADS+1}" ]; then
     export OMP_NUM_THREADS=4
 fi
+
+export OMPI_CXX=g++-mp-8
+export LUMA_CFLAGS="-O3 -std=c++0x -w -fopenmp -isysroot $(xcrun --show-sdk-path) -I/opt/local/include"
+# export LUMA_LDFLAGS="-L/opt/local/lib"
+export HDF5_HOME=/opt/local
+export LAPACK_DIR=/opt/local
+
+# TODO: this still doesn't work because LUMA needs parallel HDF5,
+# which isn't on this machine
